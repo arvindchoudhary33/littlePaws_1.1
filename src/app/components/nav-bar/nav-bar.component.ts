cfdf72884211e2dcfba78e2451b75e760ade67b3
amlp33 <sharmarahul965797@gmail.com>
2022-09-25 21:51:12 +0530

    fix ~ height bug

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+  faUsers,
+  faHouseChimneyWindow,
+  faPhone,
+} from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent implements OnInit {
+  faUsers = faUsers;
+  faHouseChimneyWindow = faHouseChimneyWindow;
+  faPhone = faPhone;
   opened = false;
   constructor() { }
 
