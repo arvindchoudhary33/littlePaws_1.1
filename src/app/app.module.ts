fb0f11d8991ccf7a9220a50152b8913285f39fe6
amlp33 <sharmarahul965797@gmail.com>
2022-09-25 17:51:21 +0530

    custom theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,11 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { FormsModule } from '@angular/forms';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @NgModule({
   declarations: [AppComponent, NavBarComponent, FooterComponent],
   imports: [
@@ -17,6 +21,11 @@ import { FormsModule } from '@angular/forms';
     MatSidenavModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent],
